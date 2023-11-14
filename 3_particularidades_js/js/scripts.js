0b2824951abb6827d44cea6abeaf25f52622bad3
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-11-14 12:06:51 -0300

    Nova Aula

--- 3_particularidades_js/js/scripts.js
+++ 3_particularidades_js/js/scripts.js
@@ -1,19 +1,28 @@
+// -------- Itens que você pode apresenta ou saber se seu codígo esta correto.
+
 console.log("Primeiro");
 
 console.log("Último");
 
+console.log("Mais um console.log");
+
+// ------- Aqui declarar uma variavel e observação na troca de valores da variavel tipadada no codigo.
+
 var a = 1;
-
-a = 'troca de valor';
-
-alert("Boto ROOOOOSA externo")
+a = 'troca de valor da variavel VAR que foi colocando com um número mais estamos apresentando a MENSAGEM da explicação.';
+console.log(a);
 
 
-//video 2:57
+// ---------------------------------------------------------
 
+// Esta // é um comentário para apensa uma linha. 
 
-function greet(){
-    console.log('Testando Function!');
-}
+/* Este /* é um comentário para mais de uma linha
 
-//var intervalId = setInterval(greet, 2000);
+Ex:
+1,
+2,
+3,
+
+*/
+
