c462e7135ece26c689cc2f17ccf0351ccc549d5c
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-11-24 11:22:21 -0300

    Criando a aula 7

---
+++ 7_boolean/js/script.js
@@ -0,0 +1,7 @@
+/* Tipos de dados: Boolean
+
+* O Boolean representa um valor verdadeiro ou falso;
+* Comparações resultam em booleans;
+* Podemos também atribuir um true ou false para uma variável e ela terá o tipo de Boolean;
+
+*/
