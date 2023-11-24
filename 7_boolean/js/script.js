df627c17ea846f1fc9bd1252082dc8546d2af759
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-11-24 17:17:39 -0300

    Aula 7

--- 7_boolean/js/script.js
+++ 7_boolean/js/script.js
@@ -5,3 +5,30 @@
 * Podemos também atribuir um true ou false para uma variável e ela terá o tipo de Boolean;
 
 */
+
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log (typeof true);
+console.log (typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+
+
+function vdd() {
+    const vd = 25;
+        if (vd >= 30) {
+            return true;
+        } else {
+            return false;
+        }
+}
+
+console.log(vdd());
+document.write(vdd());
