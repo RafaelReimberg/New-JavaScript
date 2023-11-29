b4894951d5b5742c5baf9b8c10fcab21b99e2bb9
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-11-29 18:02:53 -0300

    aula 9

---
+++ 9_objetos/js/script.js
@@ -0,0 +1,26 @@
+/* Tipos de dados: Objetos
+
+* Funcionam como uma array associativo de outras linguagens;
+* Podemos criar propriedades com chave e valor;
+* A ideia é guardar um conjunto de valores para utilizar posteriormente;
+
+*/
+var obj = {
+    nome: "Rafael",
+    idade: 36,
+    profissao: "Suporte",
+    estaTrabalhando: true,
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log("O meu nome é " + obj.nome);
+
+obj.nome = "Gabriel";
+
+console.log(obj.nome);
