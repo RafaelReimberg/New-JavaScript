41bce0987228c836d0d7a14f088011e5edd79620
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-12-05 17:34:59 -0300

    iniciado a aula 16

---
+++ 16_operador_logico_OR/js/script.js
@@ -0,0 +1,20 @@
+/* Operador lógico ||
+
+* O Operador lógico || é conhecido também como OR;
+* Ele retorna true caso uma das operações retorne verdadeiro;
+* O OR retorna false apenas se as duas expressões são falsas;
+
+*/
+
+var idade = 31;
+var nome =  "Rafaell";
+
+if(nome == "Rafael" || idade > 36){
+    console.log("Você pode soltar pipa :)");
+    document.write("Você pode soltar pipa :)");
+} else {
+    console.log("Não pode ir soltar pipa. (OBS. Muie barrou).");
+}
+
+
+/// 3:40
